4f0c12785b3dfb69daf7c5e968dfaa9211af4425
I524764 <raviganesh.maniyoor@sap.com>
2022-02-26 23:45:38 +0530

    initial portfolio frontend, backend setup

---
+++ frontend_react/src/container/Header/Header.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <div className='app__header app__flex'>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className='app__header-info'
+      >
+        <div className='app__header-badge'>
+          <div className='badge-cmp app__flex'>
+            <span>HI</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className='p-text'> Hello, I am</p>
+              <h1 className='head-text'>Raviganesh</h1>
+            </div>
+          </div>
+
+          <div className='badge-cmp app__flex'>
+            <p className='p-text'> Web Developer</p>
+            <p className='p-text'> Programmer</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className='app__header-img'
+      >
+        <img src={images.profile} alt="profile-bg" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile_circle"
+        />
+      </motion.div>
+
+      <motion.div
+      variants={scaleVariants}>
+        
+      </motion.div>
+    </div>
+  )
+}
+
+export default Header
