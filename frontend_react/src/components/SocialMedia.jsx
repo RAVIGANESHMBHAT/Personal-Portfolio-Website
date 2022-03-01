70209086737a3f882b138dc40e51fd25edb6574c
I524764 <raviganesh.maniyoor@sap.com>
2022-03-01 16:26:13 +0530

    Added portfolio contents.

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { GoMail } from 'react-icons/go';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
     <div className="app__social">
         <div>
-            <BsTwitter />
+            <GoMail />
         </div>
         <div>
             <FaFacebookF />
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
-import { AppWrap } from '../../wrapper';
-// import { AppWrap, MotionWrap } from '../../wrapper';
+import {AppWrap, MotionWrap} from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Work.scss';
 
@@ -95,14 +94,14 @@ const Work = () => {
                 </motion.div>
               </div>
 
-              {/*<div className="app__work-content app__flex">*/}
-              {/*  <h4 className="bold-text">{work.title}</h4>*/}
-              {/*  <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>*/}
+              <div className="app__work-content app__flex">
+                <h4 className="bold-text">{work.title}</h4>
+                <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
 
-              {/*  <div className="app__work-tag app__flex">*/}
-              {/*    <p className="p-text">{work.tags[0]}</p>*/}
-              {/*  </div>*/}
-              {/*</div>*/}
+                <div className="app__work-tag app__flex">
+                  <p className="p-text">{work.tags[0]}</p>
+                </div>
+              </div>
             </div>
         ))}
       </motion.div>
@@ -110,10 +109,9 @@ const Work = () => {
     </>
   )
 }
-export default Work;
-//
-// export default AppWrap(
-//     MotionWrap(Work, 'app__works'),
-//     'work',
-//     'app__primarybg',
-// );
+
+export default AppWrap(
+    MotionWrap(Work, 'app__works'),
+    'work',
+    'app__primarybg'
+);
